4a0849a29153ab06da948a8103434a25752f9edc
Caitlin <ayashi@valefor.com>
2018-08-02 20:04:01 -0700

    updating the header

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import perky from '../assets/images/perky-sm.gif';
+import corgi from '../assets/images/corgi-sm.gif';
 
 const App = () => (
     <div>
         <div className="app">
+            <img src={perky} alt="" />
             <p>WoW Pet Battle Brackets</p>
+            <img src={corgi} alt="" />
         </div>
     </div>
 );
