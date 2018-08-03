26b7edd79b2ed0c1a23dda455c67e8c088284dc1
Caitlin <ayashi@valefor.com>
2018-08-02 21:42:25 -0700

    hitting the wow api to get pet data

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../assets/css/app.css';
 
-export default props => {
-    return (
-        <div>
-            Homepage
-        </div>
-    )
+class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            pets: [],
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://us.api.battle.net/wow/pet/?locale=en_US&apikey=nsyrekgfx9hzwxkav7bzed8pxygzd7h8')
+            .then(res => res.json())
+            .then(pets => {
+                this.setState({
+                    pets: pets.pets
+                })
+            });
+    }
+
+    render() {
+        const petsArr = this.state.pets.map(pet => {
+            return <li key={pet.creatureId}>{pet.name}</li>
+        });
+        return (
+            <ul>
+                {petsArr}
+            </ul>
+        )
+    }
 }
+export default Home;
